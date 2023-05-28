25ecd64dc0a087dc8f52e92c789f585a8af42f41
DreamilyGames <dreamilygames@gmail.com>
2023-05-28 13:38:15 +0800

    Initial commit

---
+++ MyGAN/MyGAN.cpp
@@ -0,0 +1,11 @@
+// MyGAN.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "stdafx.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    return 0;
+}
